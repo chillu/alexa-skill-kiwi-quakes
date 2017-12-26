d215a1fd603595e8d03c1a8ad202f88dd19f576d
Ingo Schommer <me@chillu.com>
2017-12-27 12:01:54 +1300

    Refactored unit tests

--- test/unit/lib/select-quake.js
+++ test/unit/lib/select-quake.js
@@ -2,37 +2,50 @@ const assert = require("assert");
 const sinon = require("sinon");
 const expect = require("chai").expect;
 const selectQuake = require("../../../src/lib/select-quake");
-const fixtureQuakesNone = require("../../fixtures/quakes-none");
-const fixtureQuakesRecent = require("../../fixtures/quakes-recent");
-const fixtureQuakesOld = require("../../fixtures/quakes-old");
+const fixQuakeOldMag3 = require("../../fixtures/quake-old-mag3");
+const fixQuakeRecentMag2 = require("../../fixtures/quake-recent-mag2");
+const fixQuakeRecentMag3 = require("../../fixtures/quake-recent-mag3");
+const fixQuakeRecentMag4 = require("../../fixtures/quake-recent-mag4");
 
 const currDate = "2017-12-15T11:11:11.000Z"; // just after last quake
 
+function getFixture(quakes) {
+  return {
+    type: "FeatureCollection",
+    features: quakes
+  };
+}
+
 describe("selectQuake", function() {
   it("should tell you the most recent quake if there was one", () => {
-    return selectQuake(fixtureQuakesRecent, { currDate }).then(res => {
+    const quakes = getFixture([fixQuakeRecentMag3, fixQuakeRecentMag4]);
+    return selectQuake(quakes, { currDate }).then(res => {
       expect(res.message).to.contain("There have been 2 quakes");
     });
   });
   it("should tell you if there was no recent quake", () => {
-    return selectQuake(fixtureQuakesOld, { currDate }).then(res => {
+    const quakes = getFixture([fixQuakeOldMag3]);
+    return selectQuake(quakes, { currDate }).then(res => {
       expect(res.message).to.contain("No quakes found");
     });
   });
   it("should tell you if there was no quakes at all", () => {
-    return selectQuake(fixtureQuakesNone, { currDate }).then(res => {
+    const quakes = getFixture([]);
+    return selectQuake(quakes, { currDate }).then(res => {
       expect(res.message).to.contain("No quakes found");
     });
   });
   it("should convert relative time", () => {
+    const quakes = getFixture([fixQuakeOldMag3]);
     const since = "PT24H";
-    return selectQuake(fixtureQuakesNone, { since, currDate }).then(res => {
+    return selectQuake(quakes, { since, currDate }).then(res => {
       expect(res.message).to.contain("No quakes found since 1 day ago");
     });
   });
   it("should optionally include location", () => {
-    const latLng = { latitude: 173.8478163, longitude: -41.33280867 };
-    return selectQuake(fixtureQuakesRecent, { latLng, currDate }).then(res => {
+    const quakes = getFixture([fixQuakeRecentMag3]);
+    const latLng = { latitude: -41.33280867, longitude: 173.8478163 };
+    return selectQuake(quakes, { latLng, currDate }).then(res => {
       expect(res.message).to.contain("111 kilometers away");
     });
   });
